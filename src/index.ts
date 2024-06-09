3416a1c7c8b7a1ea835594a3903b0114f729e68f
4Sight <116753476+4S1ght@users.noreply.github.com>
2024-06-09 15:17:50 +0200

    [add] class methods & math object

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 
+// Imports ====================================================================
+
+import * as math from './math.js'
+
+// Types ======================================================================
+
 type TGildedSelectorValue = string
     | NodeList 
     | HTMLElementTagNameMap[keyof HTMLElementTagNameMap]
@@ -7,12 +13,33 @@ type TGildedSelectorValue = string
 
 type TGildedSelector = TGildedSelectorValue | Array<TGildedSelectorValue>
 
-export default function g<K extends Element>                        (selector: K | K[] | NodeListOf<K>): GildedNodeList<K>;
-export default function g<K extends keyof HTMLElementTagNameMap>    (selector: K):                       GildedNodeList<HTMLElementTagNameMap[K]>;
-export default function g<K extends keyof SVGElementTagNameMap>     (selector: K):                       GildedNodeList<SVGElementTagNameMap[K]>;
-export default function g<K extends keyof MathMLElementTagNameMap>  (selector: K):                       GildedNodeList<MathMLElementTagNameMap[K]>;
-export default function g(selector: TGildedSelector): GildedNodeList<Element> 
-{
+type S<K extends Element> = K | K[] | NodeListOf<K>
+
+
+// Static =====================================================================
+
+/**
+ * Gilded API entry point.  
+ * Holds all the statically accessible library methods, grouped under their respective categories.
+ * ```js
+ * // Access a static method
+ * g.m.rand(1, 100)
+ * 
+ * // or query for DOM elements to add a class
+ * g('div').addClass('my-div-class')
+ * 
+ * // or iterate over the queried entities
+ * for (const a of g('a')) {
+ *     console.log(a.href)
+ * }
+ * ```
+ */
+export function g<K extends Element>                        (selector: S<K>): GildedInstance<K>;
+export function g<K extends keyof HTMLElementTagNameMap>    (selector: K):    GildedInstance<HTMLElementTagNameMap[K]>;
+export function g<K extends keyof SVGElementTagNameMap>     (selector: K):    GildedInstance<SVGElementTagNameMap[K]>;
+export function g<K extends keyof MathMLElementTagNameMap>  (selector: K):    GildedInstance<MathMLElementTagNameMap[K]>;
+
+export function g(selector: TGildedSelector): GildedInstance<Element> {
 
     let items: any[] = []
     if (typeof selector === 'string')      items = Array.from(document.querySelectorAll(selector))
@@ -21,17 +48,127 @@ export default function g(selector: TGildedSelector): GildedNodeList<Element>
     else if (selector instanceof Array)    items = selector
     else throw new Error(`[Gilded] Invalid selector: ${selector} / typeof ${typeof selector}.`)
 
-    return new GildedNodeList(items)
+    return new GildedInstance(items)
 
 }
 
-class GildedNodeList<E extends Element> {
+/** Holds mathematical and color manipulation methods. */
+g.m = {
+
+    /**
+     * Returns random number within boundaries set by `min` and `max`.
+     */
+    rand: math.rand,
+
+    /**
+     * Calculates the average from an array of numbers.
+     * ```js
+     * m.avg([20, 40, 60]) // -> 40
+     * ```
+     */
+    avg: math.avg,
+
+
+    /**
+     * Caps the value in a range specified by `min` and `max`.
+     * ```js
+     * m.clamp(0, -5, 10) // 0
+     * m.clamp(0, 5, 10)  // 5
+     * m.clamp(0, 15, 10) // 10
+     * ```
+     */
+    clamp: math.clamp,
+
+    /**
+     * Returns a transition value between two numbers based on time `t`.
+     * ```js
+     * m.slide(0.5, 20, 70) // -> 45
+     * ```
+     */
+    slide: math.slide,
+
+    /**
+     * Works the same as `int.toString(16)` while making sure that the output string is at least 2
+     * characters long and works nicely when put together into a hex color code.
+     * ```js
+     * m.toHex(1)   // -> 01 (number.toString(16) would leave a single "1")
+     * m.toHex(128) // -. 80
+     * ```
+     */
+    toHex: math.toHex,
+
+    /**
+     * Takes a hex color code and converts it into the RGB CSS function format.
+     * ```js
+     * m.hexToRGB('#4cddca')   // -> rgb(75, 221, 201) 
+     * m.hexToRGB('#4cddca88') // -> rgba(75, 221, 201, 0.5) 
+     * ``` 
+     */
+    hexToRgb: math.hexToRgb,
+
+    /**
+     * Takes an RGB color function and returns a hex code.
+     * ```js
+     * m.rgbToHex('rgb(75, 221, 201)') // #4cddca
+     * ```
+     */
+    rgbToHex: math.rgbToHex,
+
+    /**
+     * Returns a transition value between two hex color codes based on the progress `t`.  
+     * Both long and shorthand hex codes are supported, as well as alpha values.
+     * ```js
+     * // Using long syntax
+     * m.hexTransform(0.5, '#000000', '#ffffff') // #808080
+     * 
+     * // Using shorthand syntax:
+     * m.hexTransform(0.5, '#000', '#fff') // #808080
+     * 
+     * // Using alpha channel:
+     * m.hexTransform(0.5, '#000000ff', '#ffffff00') // #80808080
+     * 
+     * // Mixing:
+     * // If one of the parameters is an alpha channel, the other one will also be 
+     * // treated as if it had one with opacity of 1. The resulting string will also
+     * // contain the alpha channel:
+     * m.hexTransform(0.5, '#000000aa', '#888888') // #444444d5
+     * 
+     * // Mixing (long & short syntax):
+     * m.hexTransform(0.5, '#0004', '#888888') // #444444a2
+     * ```
+     */
+    hexTransform: math.hexTransform,
+
+    /**
+     * Returns a transition value between two `rgb`/`rgba` color functions based on the progress `t`.  
+     * Both RGB and RGBA CSS functions can be used interchangeably.
+     * ```js
+     * // Using RGB:
+     * m.rgbTransform(0.5, 'rgb(0, 0, 0)', 'rgb(255, 255, 255)') // -> rgb(128, 128, 128)
+     * 
+     * // Using RGBA:
+     * m.rgbTransform(0.5, 'rgba(0, 0, 0, 1)', 'rgba(255, 255, 255, 0)') // -> rgba(128, 128, 128, 0.5)
+     * 
+     * // Mixing: 
+     * // If one of the parameters is an RGBA function, the other one will also be 
+     * // treated as RGBA with opacity of 1. The resulting string will also be in 
+     * // the RGBA format.
+     * m.rgbTransform(0.5, 'rgb(0, 0, 0)', 'rgba(255, 255, 255, 0)') // -> rgba(128, 128, 128, 0.5)
+     * ```
+     */
+    rgbTransform: math.rgbTransform
+
+}
+
+// Instance ===================================================================
+
+class GildedInstance<E extends Element> {
 
     #items: E[]
 
     constructor(items: E[]) {
         this.#items = items
-    }
+    }  
 
     [Symbol.iterator]() {
         let self = this, index = 0
@@ -41,4 +178,29 @@ class GildedNodeList<E extends Element> {
                 : { done: true }
         }
     }
+
+    // Class manipulation 
+
+    /**
+     * Adds a class name to all selected elements.
+     * ```js
+     * g('button.class#id').addClass('my-class')
+     * ```
+     */
+    public addClass(className: string | string[]): this {
+        for (let i = 0; i < this.#items.length; i++) this.#items[i].classList.add(...className)
+        return this
+    }
+
+    /**
+     * Removes a class name from selected elements.
+     * ```js
+     * g('button.class#id').removeClass('my-class')
+     * ```
+     */
+    public removeClass(className: string | string[]): this {
+        for (let i = 0; i < this.#items.length; i++) this.#items[i].classList.add(...className)
+        return this
+    }
+
 }
--- src/math.ts
+++ src/math.ts
@@ -18,61 +18,27 @@ const toLongHex = (hex: string) => {
 
 // Methods ====================================================================
 
-/**
- * Returns random number within boundaries set by `min` and `max`.
- */
-export const rand = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1) + min)
+export const rand = (min: number, max: number) => 
+    Math.floor(Math.random() * (max - min + 1) + min)
 
-/**
- * Calculates the average from an array of numbers.
- * ```js
- * m.avg([20, 40, 60]) // -> 40
- * ```
- */
 export const avg = (numbers: number[]): number => { 
     let x = 0 
     for (let i = 0; i < numbers.length; i++) x += numbers[i]
     return x / numbers.length
 }
 
-/**
- * Caps the value in a range specified by `min` and `max`.
- * ```js
- * m.clamp(0, -5, 10) // 0
- * m.clamp(0, 5, 10)  // 5
- * m.clamp(0, 15, 10) // 10
- * ```
- */
-export const clamp = (min: number, value: number, max: number): number => value < min ? min : value > max ? max : value
+export const clamp = (min: number, value: number, max: number): number => 
+    value < min ? min : value > max ? max : value
 
-/**
- * Returns a transition value between two numbers based on time `t`.
- * ```js
- * m.slide(0.5, 20, 70) // -> 45
- * ```
- */
-export const slide = (t: number, from: number, to: number): number => from + ((to - from) * t)
+export const slide = (t: number, from: number, to: number): number => 
+    from + ((to - from) * t)
 
-/**
- * Works the same as `int.toString(16)` while making sure that the output string is at least 2
- * characters long and works nicely when put together into a hex color code.
- * ```js
- * m.toHex(1)   // -> 01 (number.toString(16) would leave a single "1")
- * m.toHex(128) // -. 80
- * ```
- */
 export const toHex = (int: number): string => {
     let hex = int.toString(16)
     return hex.length === 1 ? `0${hex}` : `${hex}`
 }
 
-/**
- * Takes a hex color code and converts it into the RGB CSS function format.
- * ```js
- * m.hexToRGB('#4cddca')   // -> rgb(75, 221, 201) 
- * m.hexToRGB('#4cddca88') // -> rgba(75, 221, 201, 0.5) 
- * ``` 
- */
+// TODO: Update to use regex-only parsing
 export const hexToRgb = (hex: string): string => {
     // Remove potential hash at the start of the color code
     if (hex[0] === '#') hex = hex.substring(1)
@@ -84,12 +50,7 @@ export const hexToRgb = (hex: string): string => {
     return hexPartsNum.length === 3 ? `rgb(${hexPartsNum.join(', ')})` : `rgba(${hexPartsNum.join(', ')})`
 }
 
-/**
- * Takes an RGB color function and returns a hex code.
- * ```js
- * m.rgbToHex('rgb(75, 221, 201)') // #4cddca
- * ```
- */
+// TODO: Update to use regex-only parsing
 export const rgbToHex = (string: string): string => {
     let _string = string.replace(/rgba|rgb|\(|\)| /g, '').split(',')
     for (let i = 0; i < 3; i++) _string[i] = toHex(parseInt(_string[i]))
@@ -97,29 +58,6 @@ export const rgbToHex = (string: string): string => {
     return `#${_string.join('')}`
 }
 
-/**
- * Returns a transition value between two hex color codes based on the progress `t`.  
- * Both long and shorthand hex codes are supported, as well as alpha values.
- * ```js
- * // Using long syntax
- * m.hexTransform(0.5, '#000000', '#ffffff') // #808080
- * 
- * // Using shorthand syntax:
- * m.hexTransform(0.5, '#000', '#fff') // #808080
- * 
- * // Using alpha channel:
- * m.hexTransform(0.5, '#000000ff', '#ffffff00') // #80808080
- * 
- * // Mixing:
- * // If one of the parameters is an alpha channel, the other one will also be 
- * // treated as if it had one with opacity of 1. The resulting string will also
- * // contain the alpha channel:
- * m.hexTransform(0.5, '#000000aa', '#888888') // #444444d5
- * 
- * // Mixing (long & short syntax):
- * m.hexTransform(0.5, '#0004', '#888888') // #444444a2
- * ```
- */
 export const hexTransform = (t: number, from: string, to: string) => {
 
     const fromMatch = toLongHex(from).match(REG_HEX_REGULAR) as RegExpMatchArray
@@ -136,23 +74,6 @@ export const hexTransform = (t: number, from: string, to: string) => {
 
 }
 
-/**
- * Returns a transition value between two `rgb`/`rgba` color functions based on the progress `t`.  
- * Both RGB and RGBA CSS functions can be used interchangeably.
- * ```js
- * // Using RGB:
- * m.rgbTransform(0.5, 'rgb(0, 0, 0)', 'rgb(255, 255, 255)') // -> rgb(128, 128, 128)
- * 
- * // Using RGBA:
- * m.rgbTransform(0.5, 'rgba(0, 0, 0, 1)', 'rgba(255, 255, 255, 0)') // -> rgba(128, 128, 128, 0.5)
- * 
- * // Mixing: 
- * // If one of the parameters is an RGBA function, the other one will also be 
- * // treated as RGBA with opacity of 1. The resulting string will also be in 
- * // the RGBA format.
- * m.rgbTransform(0.5, 'rgb(0, 0, 0)', 'rgba(255, 255, 255, 0)') // -> rgba(128, 128, 128, 0.5)
- * ```
- */
 export const rgbTransform = (t: number, from: string, to: string) => {
 
     const fromMatch = from.match(REG_RGB_CSS_FUNCTION) as RegExpMatchArray
